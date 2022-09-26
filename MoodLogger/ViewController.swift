a5fabeda286b1a3ef87256453a3d31b4ccc00f08
Jack Belding <jack.belding@codagami.com>
2022-09-26 18:59:46 -0400

    first commit

---
+++ MoodLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodLogger
+//
+//  Created by Jack Belding on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
